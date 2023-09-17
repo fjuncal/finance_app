86952e33d850f3205a1b82bda2e94342567af148
Fellipe Juncal <fjuncal7@gmail.com>
2023-09-17 02:00:54 -0300

    adicionando fonte e foto no projeto

--- lib/common/constants/app_text_styles.dart
+++ lib/common/constants/app_text_styles.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppTextStyles {
   AppTextStyles._();
 
-  static const TextStyle bigText = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w700);
+  static const TextStyle bigText = TextStyle(fontFamily: 'Inter',  fontSize: 50.0, fontWeight: FontWeight.w700);
 }
